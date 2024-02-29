394f20df72f05ea6201dbbfc643baabc66e79572
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-02-29 17:32:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,14 @@
 #
-# Copyright (C) 2023 LineageOS
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_yunluo.mk
+    $(LOCAL_DIR)/twrp_yunluo.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach FLAVOR, user userdebug eng, lineage_yunluo-$(FLAVOR))
+    twrp_yunluo-user \
+    twrp_yunluo-userdebug \
+    twrp_yunluo-eng
